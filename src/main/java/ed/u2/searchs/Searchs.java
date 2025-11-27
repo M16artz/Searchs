64cb9ad8a14bd7fdd3943cbd27b36916cefb0df4
M16artz <72704769+M16artz@users.noreply.github.com>
2025-11-27 07:52:17 -0500

    Add files via upload

---
+++ src/main/java/ed/u2/searchs/Searchs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.u2.searchs;
+
+/**
+ *
+ * @author PC
+ */
+public class Searchs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
